4f814c4bf7aedc2b4f1a73cdd15bb1a2a178529d
SagiBL <sagi.benlulu@gmail.com>
2025-01-29 16:07:07 +0200

    Update student.h

--- hw3/student.h
+++ hw3/student.h
@@ -5,19 +5,14 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef void *elem_t;
-typedef void* (*clone_t)(elem_t e);
-typedef void (*destroy_t)(elem_t e);
-typedef void (*print_t)(elem_t e);
-
 struct student {
 char *name;
 int age;
 int id;
 };
 
-struct student* student_clone(struct student* student);
-void student_destroy(struct student* student);
-void student_print(struct student* student);
+void* student_clone(void* student);
+void student_destroy(void* student);
+void student_print(void* student);
 
 #endif
