934736f45a552eeaf461b1da49f13a9a7a54ec67
SagiBL <sagi.benlulu@gmail.com>
2025-01-29 10:28:51 +0200

    Update student.c

--- hw3/student.c
+++ hw3/student.c
@@ -3,51 +3,48 @@
 #include <string.h>
 #include "student.h"
 
-
-
-struct student {
-char *name;
-int age;
-int id;
-};
-
-struct student* student_clone(struct student* student){
+// Function to clone a student
+void* student_clone(void* student_void) {
+    struct student* student = (struct student*)student_void;
     struct student* clone = (struct student*)malloc(sizeof(struct student));
     if (clone == NULL) {   
-        return NULL;        //failed
+        return NULL;        // failed
     }
 
     clone->age = student->age;
     clone->id = student->id;
 
-    // memory allocation for clone->name
+    // Memory allocation for clone->name
     if (student->name != NULL) {
         clone->name = (char*)malloc(strlen(student->name) + 1);  
         if (clone->name != NULL) {
             strcpy(clone->name, student->name);  // Copy name
         } 
         else {
-            free(clone);  // free the struct
-            return NULL;   //failed
+            free(clone);  // Free the struct
+            return NULL;   // failed
         }
     } 
     else {
-        clone->name = NULL;  // original name is NULL => clone name is NULL
+        clone->name = NULL;  // Original name is NULL => clone name is NULL
     }
 
     return clone;
 }
 
-
-void student_destroy(struct student* student){
-    if(student->name != NULL) {     // Free the dynamically allocated memory for name (if it was allocated)
+// Function to destroy a student
+void student_destroy(void* student_void) {
+    struct student* student = (struct student*)student_void;
+    if(student->name != NULL) {     // Free dynamically allocated memory for name (if allocated)
         free(student->name);
     }
     free(student);                  // Free the memory for the struct itself
     return;
 }
 
-void student_print(struct student* student){
-    printf("student name: %s, age: %d, id: %d\n", student->name,student->age,student->id);
+// Function to print a student
+void student_print(void* student_void) {
+    struct student* student = (struct student*)student_void;
+    printf("student name: %s, age: %d, id: %d\n", student->name, student->age, student->id);
     return;
 }
